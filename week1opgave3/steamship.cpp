4553ceb8a91574672464bdf6027c089875a0229b
Sam <sam.giezeman@gmail.com>
2017-05-04 15:32:04 +0200

    steamship cpp/hpp aangepast zodat de class steamship alle objecten bevat en niet alleen een print geeft

--- week1opgave3/steamship.cpp
+++ week1opgave3/steamship.cpp
@@ -25,19 +25,7 @@
  *  this function requires 2 starting coordinates (x and y) and a bool to decide wether it needs to print a version with or without clouds
  *  True is given for the version with clouds and a second chimney.
  */
-void giveMeABoat::print(){
-    line water(w,start_x,start_y+50, start_x+128, start_y+50);
-    line boatDeck(w, start_x+10, start_y+40, start_x+100, start_y+40);
-    line boatRear(w, start_x+100, start_y+40, start_x+80, start_y+50);
-    line boatStern(w, start_x+30, start_y+50, start_x+10, start_y+40);
-    rectangle firstChimney(w, start_x+60, start_y+30, start_x+65, start_y+40);
-    rectangle secondChimney(w, start_x+70, start_y+33, start_x+75, start_y+40);
-    circle cloud0(w, start_x+75, start_y+ 25, 3);
-    circle cloud1(w, start_x+81, start_y+ 19, 3);
-    circle cloud2(w, start_x+86, start_y+ 14, 2);
-    circle cloud3(w, start_x+92, start_y+ 10, 2);
-    circle cloud4(w, start_x+98, start_y+ 8, 2);
-    
+void steamship::print(){
     boatStern.print();
     water.print();
     boatRear.print();
--- week1opgave3/steamship.hpp
+++ week1opgave3/steamship.hpp
@@ -17,23 +17,48 @@
 #define STEAMSHIP_HPP
 
 #include "window.hpp"
+#include "line.hpp"
+#include "rectangle.hpp"
+#include "circle.hpp"
 
 /**
- * @brief defines the class giveMeABoat
- * (wether it is a ship or a boat i actually don't know.)
+ * @brief defines the class steamship
+ * it contains lines, rectangles, circles and a window 
  */
-class giveMeABoat{
+class steamship{
     
     private:
     window & w;
     int start_x, start_y;
     bool clouds;
+    line  water;
+    line boatDeck;
+    line boatRear;
+    line boatStern;
+    rectangle firstChimney;
+    rectangle secondChimney;
+    circle cloud0;
+    circle cloud1;
+    circle cloud2;
+    circle cloud3;
+    circle cloud4;
     public:
-    giveMeABoat(window & w, int start_x,int start_y, bool clouds): //prints a steam boat with puffy clouds from the second chimney
+    steamship(window & w, int start_x,int start_y, bool clouds): //prints a steam boat with puffy clouds from the second chimney
         w(w),
         start_x(start_x),
         start_y(start_y),
-        clouds(clouds)
+        clouds(clouds),
+        water(w,start_x,steamship::start_y+50, steamship::start_x+128, steamship::start_y+50),
+        boatDeck(w, steamship::start_x+10, steamship::start_y+40, steamship::start_x+100, steamship::start_y+40),
+        boatRear(w, steamship::start_x+100, steamship::start_y+40, steamship::start_x+80, steamship::start_y+50),
+        boatStern(w, steamship::start_x+30, steamship::start_y+50, steamship::start_x+10, steamship::start_y+40),
+        firstChimney(w, steamship::start_x+60, steamship::start_y+30, steamship::start_x+65, steamship::start_y+40),
+        secondChimney(w, steamship::start_x+70, steamship::start_y+33, steamship::start_x+75, steamship::start_y+40),
+        cloud0(w, steamship::start_x+75, steamship::start_y+ 25, 3),
+        cloud1(w, steamship::start_x+81, steamship::start_y+ 19, 3),
+        cloud2(w, steamship::start_x+86, steamship::start_y+ 14, 2),
+        cloud3(w, steamship::start_x+92, steamship::start_y+ 10, 2),
+        cloud4(w, steamship::start_x+98, steamship::start_y+ 8, 2)
         {}
     void print();
 };
